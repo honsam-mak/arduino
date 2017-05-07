3b80a5be25aef94dee7d6121e43c86e624770a44
Honsam Mak <honsam.mak@maksapplab.com>
2017-05-07 22:39:48 +0800

    move definition into header

---
+++ ESPDuino_U_Max6675/ESPDuino_U_Max6675.h
@@ -0,0 +1,31 @@
+// Common setting
+#define DELAY_DURATION 1000
+#define BAUD_MONITOR  9600
+
+// AP setting
+#define SSID        "mark"
+#define PASSWORD    "6266790021"
+
+// ThingSpeak setting
+#define TS_IP       "184.106.153.149" // api.thingspeak.com
+#define WRITE_API_KEY   "6RZFFXN81EIFD1XK"
+#define PORT 80
+#define WAIT_SEC 16
+
+// ESP 8266 setting
+#define BAUD_ESP8266  115200    // Not used
+
+// LCD setting
+#define COL 16
+#define ROW 2
+
+// Pin setting
+#define RTC_RST 16
+#define RTC_DAT 15
+#define RTC_CLK 14
+// SDA: 4 (default) or 2, SCL: 5 (default) or 14
+#define LCD_SDA 4
+#define LCD_SCL 5
+#define TMP_S0  13
+#define TMP_CS  12 
+#define TMP_SCK 2
--- ESPDuino_U_Max6675/ESPDuino_U_Max6675.ino
+++ ESPDuino_U_Max6675/ESPDuino_U_Max6675.ino
@@ -6,8 +6,8 @@
 
  /*
   * Issues:
-  *   1. LCD and Wifi don't work together. Wifi won't get connected. (fixed : add disconnect)
-  *   2. MAX6675 and Wifi don't work together. Temperature always get 0 degree.
+  *   1. LCD and Wifi don't work together. Wifi won't get connected. (fixed: add disconnect)
+  *   2. MAX6675 and Wifi don't work together. Temperature always get 0 degree. (fixed: Don't use pin 1,3)
   */
   
 #include <ESP8266WiFi.h> 
@@ -15,38 +15,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <max6675.h>
+#include "ESPDuino_U_Max6675.h"
 
-// Common setting
-#define DELAY_DURATION 1000
-#define BAUD_MONITOR  9600
-
-// AP setting
-#define SSID        "mark"
-#define PASSWORD    "6266790021"
-
-// ThingSpeak setting
-#define TS_IP       "184.106.153.149" // api.thingspeak.com
-#define WRITE_API_KEY   "6RZFFXN81EIFD1XK"
-#define PORT 80
-#define WAIT_SEC 16
-
-// ESP 8266 setting
-#define BAUD_ESP8266  115200    // Not used
-
-// LCD setting
-#define COL 16
-#define ROW 2
-
-// Pin setting
-#define RTC_RST 16
-#define RTC_DAT 15
-#define RTC_CLK 14
-// SDA: 4 (default) or 2, SCL: 5 (default) or 14
-#define LCD_SDA 4
-#define LCD_SCL 5
-#define TMP_S0  13
-#define TMP_CS  12 
-#define TMP_SCK 2
 
 
 DS1302 rtc(RTC_RST, RTC_DAT, RTC_CLK);
